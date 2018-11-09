9ddb5f4c7c99d2574af6ccb98976a94d1fa7d173
sohe1l <soheilansari@icloud.com>
2018-11-08 19:26:16 -0800

    Set up main view

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
+
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordButton: UIButton!
+    @IBOutlet weak var recordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        stopRecordButton.isEnabled = false;
+    }
 
+    
+    @IBAction func recordButtonPressed(_ sender: Any) {
+        print("recordButtonPressed")
+    }
+    
+    
+    @IBAction func stopButtonPressed(_ sender: Any) {
+        print("stopButtonPressed")
+    }
+    
+    
+    
 
 }
 
